36aa1f87a4343886dc596f3dc9060208dfd87c52
thanhvc <thanhvc@exoplatform.com>
2014-01-02 17:10:01 +0700

    SOC-4034 | [Activity] Removing thread safe handling when creates Activity

--- component/core/src/test/java/org/exoplatform/social/core/storage/impl/ActivityStorageImplTestCase.java
+++ component/core/src/test/java/org/exoplatform/social/core/storage/impl/ActivityStorageImplTestCase.java
@@ -312,7 +312,7 @@ public class ActivityStorageImplTestCase extends AbstractCoreTest {
 
   }
 
-  @MaxQueryNumber(264)
+  @MaxQueryNumber(280)
   public void testSaveComment() throws Exception {
 
     //
@@ -579,7 +579,7 @@ public class ActivityStorageImplTestCase extends AbstractCoreTest {
     assertEquals(false, updatedActivity.isLocked());
   }
 
-  @MaxQueryNumber(18000)
+  @MaxQueryNumber(21600)
   public void testCommentOrder() throws Exception {
     // fill 10 activities
     for (int i = 0; i < 10; ++i) {
@@ -612,7 +612,7 @@ public class ActivityStorageImplTestCase extends AbstractCoreTest {
     }
   }
 
-  @MaxQueryNumber(1700)
+  @MaxQueryNumber(2200)
   public void testDeleteComment() throws Exception {
     ExoSocialActivity activity = new ExoSocialActivityImpl();
     activity.setTitle("activity title");
@@ -709,7 +709,7 @@ public class ActivityStorageImplTestCase extends AbstractCoreTest {
 
   }
 
-  @MaxQueryNumber(268)
+  @MaxQueryNumber(280)
   public void testTimeStamp() throws Exception {
     ExoSocialActivity activity = new ExoSocialActivityImpl();
     activity.setTitle("activity title");
@@ -932,7 +932,7 @@ public class ActivityStorageImplTestCase extends AbstractCoreTest {
     
   }
 
-  @MaxQueryNumber(264)
+  @MaxQueryNumber(280)
   public void testActivityProcessing() throws Exception {
 
     //
